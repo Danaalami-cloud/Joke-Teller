bb7ccbf498f7b291b1400475e4b9799c9a37a317
Dana Alami <dana.alami18@gmail.com>
2023-05-31 15:26:38 +0100

    finished joke proj

---
+++ script.js
@@ -0,0 +1,48 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+// Disable/Enable button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+
+// Passing our jokes to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
+        key: '41aa895b412f46189be6314b9222a907',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+// Get Jokes from jokes API
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&idRange=0-185';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable button
+        toggleButton();
+    } catch (error) {
+        // Catch errors here
+        console.log('whoops', error);
+    }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
